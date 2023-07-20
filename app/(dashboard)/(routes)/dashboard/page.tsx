6e5c3572f6bb021c5169d4cf581c0e37cc5406fb
Drakkarrr <junrey404@gmail.com>
2023-07-21 07:53:43 +0800

    Feat: added signout auth and user profile feature

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,11 +1,10 @@
+import { UserButton } from '@clerk/nextjs';
+
 const DashboardPage = () => {
   return (
     <>
-      <h1>DashboardPage pageeee(Protected)</h1>
-      <h1>DashboardPage pageeee(Protected)</h1>
-      <h1>DashboardPage pageeee(Protected)</h1>
-      <h1>DashboardPage pageeee(Protected)</h1>
-      <h1>DashboardPage pageeee(Protected)</h1>
+      <h1>Welcome to DashboardPage pageeee(Protected)</h1>
+      <UserButton afterSignOutUrl='/' />
     </>
   );
 };
