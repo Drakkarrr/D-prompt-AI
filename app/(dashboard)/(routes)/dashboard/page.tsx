a5db7ade98fbbf01b7b81e5dc29841145627f63f
Drakkarrr <junrey404@gmail.com>
2023-07-20 09:50:40 +0800

    added middleware and dashboard

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+const DashboardPage = () => {
+  return (
+    <>
+      <h1>DashboardPage pageeee(Protected)</h1>
+    </>
+  );
+};
+
+export default DashboardPage;
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,9 @@
+const LandingPage = () => {
+  return (
+    <>
+      <h1>This is a Landing Page (unprotected)</h1>
+    </>
+  );
+};
+
+export default LandingPage;
