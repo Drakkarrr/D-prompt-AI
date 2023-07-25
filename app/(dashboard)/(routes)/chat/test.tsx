6aa2ee8f8227fb9197406f458ed73dae297e5a9f
Drakkarrr <junrey404@gmail.com>
2023-07-25 14:27:31 +0800

    Fixed hydration issue

--- app/(dashboard)/(routes)/chat/test.tsx
+++ app/(dashboard)/(routes)/chat/test.tsx
@@ -1,23 +1,32 @@
 'use client';
 
-import { useState } from 'react';
-import { useRouter } from 'next/navigation';
-
-import Heading from '@/components/Heading';
-import { Form, FormControl, FormField, FormItem } from '@/components/ui/form';
-import { Input } from '@/components/ui/input';
-import { Button } from '@/components/ui/button';
-
-import { useForm } from 'react-hook-form';
-import { z } from 'zod';
-import { zodResolver } from '@hookform/resolvers/zod';
-
+import * as z from 'zod';
+import axios from 'axios';
 import { MessageSquare } from 'lucide-react';
-import { formSchema } from './constants';
-import { cn } from '@/lib/utils';
+import { useForm } from 'react-hook-form';
+import { useState } from 'react';
+import { toast } from 'react-hot-toast';
+import { useRouter } from 'next/navigation';
+import { ChatCompletionRequestMessage } from 'openai';
 
-const ChatPage = () => {
+import { BotAvatar } from '@/components/bot-avatar';
+import { Heading } from '@/components/heading';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Form, FormControl, FormField, FormItem } from '@/components/ui/form';
+import { cn } from '@/lib/utils';
+import { Loader } from '@/components/loader';
+import { UserAvatar } from '@/components/user-avatar';
+import { Empty } from '@/components/ui/empty';
+import { useProModal } from '@/hooks/use-pro-modal';
+
+import { formSchema } from './constants';
+
+const ConversationPage = () => {
   const router = useRouter();
+  const proModal = useProModal();
+  const [messages, setMessages] = useState<ChatCompletionRequestMessage[]>([]);
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -28,25 +37,47 @@ const ChatPage = () => {
 
   const isLoading = form.formState.isSubmitting;
 
-  const handleSubmit = async (data: z.infer<typeof formSchema>) => {
-    console.log(data);
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      const userMessage: ChatCompletionRequestMessage = {
+        role: 'user',
+        content: values.prompt,
+      };
+      const newMessages = [...messages, userMessage];
+
+      const response = await axios.post('/api/conversation', {
+        messages: newMessages,
+      });
+      setMessages((current) => [...current, userMessage, response.data]);
+
+      form.reset();
+    } catch (error: any) {
+      if (error?.response?.status === 403) {
+        proModal.onOpen();
+      } else {
+        toast.error('Something went wrong.');
+      }
+    } finally {
+      router.refresh();
+    }
   };
 
   return (
-    <>
+    <div>
       <Heading
-        title='Chat with D-prompt AI'
-        description='Chat with the most advance chat AI model'
+        title='Conversation'
+        description='Our most advanced conversation model.'
         icon={MessageSquare}
-        color='text-blue-500'
-        bgColor='bg-blue-500/10'
+        iconColor='text-violet-500'
+        bgColor='bg-violet-500/10'
       />
 
+      {/* Test */}
       <div className='px-4 lg:px-8'>
         <div>
           <Form {...form}>
             <form
-              onSubmit={form.handleSubmit(handleSubmit)}
+              onSubmit={form.handleSubmit(onSubmit)}
               className='grid w-full grid-cols-12 gap-2 rounded-lg border p-4 px-3 focus-within:shadow-sm md:px-6'
             >
               <FormField
@@ -77,12 +108,8 @@ const ChatPage = () => {
         </div>
 
         <div className='mt-4 space-y-4'>
-          <p>Contents here</p>
-        </div>
-
-        {/* <div className='mt-4 space-y-4'>
           {isLoading && (
-            <div className='flex items-center justify-center w-full p-8 rounded-lg bg-muted'>
+            <div className='flex w-full items-center justify-center rounded-lg bg-muted p-8'>
               <Loader />
             </div>
           )}
@@ -105,10 +132,10 @@ const ChatPage = () => {
               </div>
             ))}
           </div>
-        </div> */}
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
-export default ChatPage;
+export default ConversationPage;
