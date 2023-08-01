5ddde38a0405abb756f295d6083d94fd9dd91721
Drakkarrr <junrey404@gmail.com>
2023-08-01 10:12:44 +0800

    added zustand and modal state provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
 import { ClerkProvider } from '@clerk/nextjs';
+import { ModalProvider } from '@/components/ModalProvider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -20,7 +21,10 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang='en'>
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+         <ModalProvider />
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
---
+++ components/ModalProvider.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import ProModal  from "@/components/ProModal";
+
+
+export const ModalProvider = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) return null;
+
+  return (
+    <>
+      <ProModal />
+    </>
+  );
+};
