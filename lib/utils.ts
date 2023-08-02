180d388ed1f0e3545a766a37c25e16805b23ba50
Drakkarrr <junrey404@gmail.com>
2023-08-02 16:36:16 +0800

    added stripe integration and addtional utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,15 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
- 
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function absoluteUrl(path: string) {
+  return `${process.env.NEXT_PUBLIC_HOST}${path}`
+}
+
+// export const absoluteUrl = (path: string) => {
+//   const url = new URL(path, process.env.NEXT_PUBLIC_HOST ?? "https://geniusly.ai")
+//   return url.toString()
+// }
