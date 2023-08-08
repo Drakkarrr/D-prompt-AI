0a4e9e08697ffbd736579a070e9cb3a2c2253cf1
Drakkarrr <junrey404@gmail.com>
2023-08-08 11:13:36 +0800

    added server components settings page

---
+++ app/(dashboard)/(routes)/settings/page.tsx
@@ -0,0 +1,26 @@
+import Heading from '@/components/Heading';
+import { getSubscription } from '@/lib/subscription';
+import { Settings } from 'lucide-react';
+
+const SettingsPage = async () => {
+  const isPro = await getSubscription();
+
+  return (
+    <>
+      <Heading
+        title='Settings'
+        description='This is the settings page'
+        icon={Settings}
+        color='text-gray-500'
+        bgColor='bg-gray-700/10'
+      />
+      <div className='space-y-4 px-4 lg:px-8'>
+        <div className='text-sm text-muted-foreground'>
+          {isPro ? 'You are a pro user' : 'You are not a pro user'}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SettingsPage;
